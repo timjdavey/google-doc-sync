cfa69f18fb9badc514c52f492dfbf0af1b1c818c
timjdavey <timjdavey@gmail.com>
2011-05-24 11:01:04 +0100

    redoing docs

--- README.md
+++ README.md
@@ -1,1 +1,117 @@
-How to set up a google doc
+##Description
+
+A small helper tool to sync a list of objects backed in a typical database, json, csv format with a google spreadsheet. Currently it only supports push sync 
+    
+Two main common use cases are
+* Augment existing data in a flexible way. Add and append extra data columns to the object being sent over.
+* BI tool. Use Google's built in filter, graphs, charts & formula functions to cut up the data how the analysis likes.
+
+There are however two prerequesist to using this tool
+* The list of objects must be convertable into a dictionary which supports only key value string pairs (e.g. no sub lists unless using csv)
+* Each object must have a unique primary key
+
+
+##Example
+
+###Setting up the spreadsheet
+Setting up and extending the base ``GoogleSpreadsheet`` class
+        
+    class ExampleUserSheet(GoogleSpreadsheet):
+        primary_key = 'pk'
+        
+        def __init__(self, cache_feed=False):
+            # optionally can preprogram creditials into sheet
+            self.email = settings.GOOGLE_SPREADSHEET_EMAIL
+            self.password = settings.GOOGLE_SPREADSHEET_PASSWORD
+            self.spreadsheet = 'h8fw4kbaeflnafaf'
+            self.worksheet = 'od6'
+            self.source = 'server'
+            self.cache_feed = cache_feed
+        
+        def convert(self, payload):
+            converted = {
+                'pk': payload.pk,
+                'username': payload.username,
+                'email: payload.email,
+            }
+            return converted
+        
+Where there are three important steps here.
+
+**First.** you need to set the ``primary_key`` which identifies the spreadsheet entry to the payload object's database equivalent. This value much be unique. So for the example above the ``payload`` is a ``User`` object with its ``primary_key`` being the ``pk`` (django example).
+
+**Second.** define a ``convert`` function. It takes a payload
+object and returns a dictionary where the keys are the google column header's you define. *Watch out* The value will be cohereced to a string when sent to google, so be careful that the __str__ is exactly what you want to be sent. So with that Google has some general restrictions. For key names it removes all spaces and underscores - so make sure your dictionary has those removed. While for values it turns entries like '006' to just 6.
+
+**Third.** setup the actual google spreadsheet and store the creditials. For this simply create a spreadsheet in some users name. Once created in the URL you'll see ``key=`` which is the spreadsheet key which needs setting above. Working out the worksheet id is a little trickier, but for now its always ``od6`` for the first sheet. Throw these creditials in like above. The optional ``source`` value is for audit purposes. When setting up the google doc, simply put in the column headers at the top line of the doc, with the primary_key set as the first column.
+
+There is a final optional **fourth** parameter called ``cache_feed`` which if ``True`` will cache the feed brought back from google on a per sheet basis. We talk about that later.
+
+
+###Interacting with the spreadsheet
+When creating or updating a new user on the fly, you can insert it on an ad-hoc basis. Or of course delete it as well.
+    
+    # when updating a user, or can override the user save function
+    user.save()
+    urow = ExampleUserSheet().get(user)
+    urow.save()
+    
+    # or similiarly if you delete
+    user.delete()
+    urow.delete()
+    
+
+Or when doing a bulk sync, you can do a bit more advanced logic.
+        
+    sheet = ExampleUserSheet()
+    
+    users = User.objects.all()
+    for u in users:
+        row = sheet.get(u)
+        if u.is_active:
+            row.save()
+        else:
+            if row.exists(): # if it exists remotely
+                row.delete() # then must remove as only want active
+
+However, the code is incredibly slow as on each ``get`` it gets the entire spreadsheet fresh & rebuilds the rows. This is good as changes may have been made which you don't want overridden. However, you can also cache the feed.
+    
+    sheet = ExampleUserSheet(cache_feed=True)
+    users = User.objects.all()
+    for u in users:
+       row = sheet.get(u)
+       row.save(refresh=True)
+
+By setting ``refresh=True`` on the row save, despite the fact cache_feed is set before sending any changes to the spreadsheet it will forcely get the latest from the spreadsheet. There is a possibility that you miss updates that should happen, however your won't accidentaly overwrite good data and you'll  massively improve performance.
+
+If there are corruptions in the sheet causing there to be multiple entries per pk (if say they were manually entered by a human).
+
+    try:
+        sheet.get(u)
+    except MutipleEntriesExist:
+        sheet.deduplicate()
+
+Deduplicate will unintelligently remove all but the first entries it finds. This can be overridden by smarter functions which look at a last_updated column or the equivelent value stored on the google entry which is returned. The current function returns a list of pks which were removed.
+
+
+###Experimental feeding back to database:
+
+This is experimental. Obviously very dangerous & probably massively error prone. However, if you want to live dangerously, here's how...
+    
+    def convert_back(self):
+        # payload is User with correct pk, could defined model & lookup?
+        for k, v in self.incoming().items():
+            # inreality will need some cohercing into correct keys & types
+            setattr(self.payload, k, v)
+        self.payload.save()
+    
+    # another example
+    def convert_back(self):
+        User.objects.filter(pk=self.pk).update(**self.incoming())
+
+Define this on the row and simply call it after doing a 
+
+
+##TODO
+Need to add logging. However want to add in the new django logging but don't want to spoil it for the rest. Then also tossing up setting up leveling.
+
--- spreadsheet.py
+++ spreadsheet.py
@@ -1,142 +1,3 @@
-"""
-Description:
-
-    A small helper tool to sync a list of objects backed in a typical
-    database, json, csv format with a google spreadsheet. Currently it only
-    supports push sync 
-    
-    Two main common use cases are
-        # Augment existing data in a flexible way. Add and append extra 
-        data columns to the object being sent over.
-        # BI tool. Use Google's built in filter, graphs, charts & formula
-        functions to cut up the data how the analysis likes.
-    
-    There are only two prerequesist to using this tool. The list of objects
-    must be convertable into a dictionary which supports only key value string
-    pairs. Therefore its difficult to store sub-lists against the items unless
-    csv is good enough.
-
-
-Example Definition:
-    First you need to extend the ``GoogleSpreadsheet`` with two parts.::
-        
-        class ExampleUserSheet(GoogleSpreadsheet):
-            primary_key = 'pk'
-            
-            def __init__(self, cache_feed=False):
-                # optionally can preprogram creditials how you like
-                self.email = settings.GOOGLE_SPREADSHEET_EMAIL
-                self.password = settings.GOOGLE_SPREADSHEET_PASSWORD
-                self.spreadsheet = 'h8fw4kbaeflnafaf'
-                self.worksheet = 'od6'
-                self.source = 'server'
-                self.cache_feed = cache_feed
-            
-            def convert(self, payload):
-                converted = {
-                    'pk': payload.pk,
-                    'username': payload.username,
-                    'email: payload.email,
-                }
-                return converted
-            
-    Where there are two important parts here. Firstly you need to set
-    the primary_key which identifies the spreadsheet entry to the payload
-    objects database equivalent. This value much be unique.
-    
-    The second is to define a ``convert`` function, which takes a payload
-    object (or any definition of your choosing) and returns a dictionary.
-    Keyed to the google column header you define. The value will be cohereced
-    to a string when sent to google, so be careful that the __str__ is how
-    you expect.
-    
-    Google has some restrictions on key names. It seems to remove all spaces
-    and underscores. While for values turns values like '006' to just 6.
-    
-    There is a final parameter called ``cache_feed`` which if ``True`` will
-    cache the feed brought back from google. If you are writing back, it is
-    highly recommened that this is set to ``False`` otherwise you are
-    increasing the risk that changes made to the spread sheet will be
-    overridden (even those which are not payload fields). If you are simply
-    reading from 
-
-
-Example Usage:
-    When creating or updating a new user insert indiviually::
-    
-        # when updating a user, or can override the user save function
-        user.save()
-        urow = ExampleUserSheet().get(user)
-        urow.save()
-        
-        # or similiarly if you delete
-        user.delete()
-        urow.delete()
-    
-    When updating a sheet can reuse the sheet::
-        
-        sheet = ExampleUserSheet()
-        
-        users = User.objects.all()
-        for u in users:
-            row = sheet.get(u)
-            if u.is_active:
-                # will only update if they are out of date
-                # it will also not create two entries if pk's are correct
-                row.save()
-            else:
-                if row.exists(): # if it exists remotely
-                    row.delete() # then must remove as only want active
-    
-    
-    When updating in bulk, can use cache lazily::
-    
-        sheet = ExampleUserSheet(cache_feed=True)
-        
-        users = User.objects.all()
-        for u in users:
-            row = sheet.get(u)
-            row.save(refresh=True)
-    
-    By setting ``refresh=True`` on save, if the row is out_of_sync, it will
-    force the sheet to update with the latest feed (entries). This ensures
-    that any writes check for the latest first. There is a possibility that
-    you miss updates that should happen, however you also don't need to hit
-    google to check each time - massively improving performance.
-    
-    If there are corruptions in the sheet (multiple entries per pk). A
-    ``MutipleEntriesExist`` exception will be thrown. These can be tieded
-    up using ``sheet.deduplicate()``, which is unintelligently remove all but
-    the first entries it finds. This can be overridden by smarter functions
-    which look at a last_updated column or the equivelent value stored on the
-    google entry which is returned. The current function returns a list of
-    pks which were removed.
-
-
-Experimental feed back to database:
-
-    This is experimental. Obviously very dangerous & probably massively
-    error prone. However, if you want to live dangerously, here's how::
-    
-        def convert_back(self, row, payload):
-            # payload is User with correct pk, could defined model & lookup?
-            for k, v in row.incoming().items():
-                # inreality will need some cohercing into correct keys & types
-                setattr(payload, k, v)
-            payload.save()
-            
-            # or another example
-            User.objects.filter(pk=row.pk).update(**row.incoming)
-
-TODO:
-
-    * add logging
-
-
-THIS IS NEW! I did this not so long ago and it was fine
-
-
-"""
 import gdata.spreadsheet
 import gdata.spreadsheet.service
 import gdata.service
@@ -153,7 +14,8 @@ class MutipleEntriesExist(Exception):
 
 class GoogleRow(object):
     """Helper object to pass info. Please see docs for usage."""
-    def __init__(self, data, entry, sheet):
+    def __init__(self, payload, data, entry, sheet):
+        self.payload = payload
         self.converted = self.clean(data)
         self._entry = entry # underscored for proper error handling
         self.sheet = sheet
@@ -298,7 +160,7 @@ class GoogleSpreadsheet(object):
             entry = self.entry(pk)
         except EntryDoesNotExist:
             entry = None
-        return GoogleRow(data, entry, self)
+        return GoogleRow(payload, data, entry, self)
     
     def create(self, row):
         """ Creates a row in Google, making sure it doesn't already exist """
